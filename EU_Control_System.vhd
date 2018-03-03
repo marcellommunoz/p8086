4dd693f5a72c5f8845d9af218b6c5eaec10d6a5e
gabrielsosoares <gsdsoares@inf.ufpel.edu.br>
2018-03-03 18:14:16 -0300

    ECL

--- EU_Control_System.vhd
+++ EU_Control_System.vhd
@@ -6,12 +6,16 @@ entity EU_Control_System is
   port (
     reset 													: in std_logic;
     clk     												: in std_logic;
+	 --Queue vazia
+	 QueueVazia												: in std_logic;
 	 --instrucao recebida
     entradaInstrucao 									: in  std_logic_vector(7 downto 0);
 	 --Entrada Registrador Temporarios
 	 EntradaRT1, EntradaRT2							: in std_logic_vector(15 downto 0);
 	 --entrada e saidas dos RG
 	 entradaRG1, entradaRG2, saidaRG1, saidaRG2	: out std_logic_vector(3 downto 0);
+	 --Sinal de leitura da queue
+	 LeituraQueue											: out std_logic;
 	 --sinal de escrita para o RG
 	 sinalEscritaRG1, sinalEscritaRG2 				: out std_logic;
 	 --saida do ADB
@@ -23,6 +27,7 @@ entity EU_Control_System is
 	 --OP da ULA
 	 --Sinal Escrita Flag
 	 WFlag													: out std_logic;
+	 IncrementaPC											: out std_logic;
 	 OPULA													: out std_logic_vector(7 downto 0);
 	 --Memory
 	 saidaBR1, saidaBR2, saidaBR3						: out std_logic_vector(2 downto 0)
@@ -30,8 +35,10 @@ entity EU_Control_System is
 end EU_Control_System;
  
 architecture description of EU_Control_System is
-	TYPE State_type IS (fetch, readin, writeback, op, boot, arithmetic, exe);
+	TYPE State_type IS (fetch, readin, writeback, op, boot, arithmetic161, arithmetic162, exe, resposta, final);
 	SIGNAL state : State_Type;
+	signal destino : std_logic_vector(3 downto 0);
+	signal instrucaoReal : std_logic_vector(7 downto 0);
 --controle sub add
 --instrucao 8 bits
 
@@ -40,203 +47,181 @@ begin
  
 	EU_CONTROL : process (clk, reset) is
 	variable instrucaoAtual : std_logic_vector(7 downto 0);
+	variable destinofonte : std_logic_vector(3 downto 0);
 		begin
 		if (reset = '1') then
 			state <= boot;
 		elsif clk'event and clk = '1' then
 			case state is
 				when boot =>
-					
-				when fetch =>
-					--AAA
-					if(entradaInstrucao = "00000001") then
-						instrucaoAtual  := entradaInstrucao;
-						state <= readin;
-					elsif (entradaInstrucao = "01010000" or entradaInstrucao = "11010000") then --ADD
-						instrucaoAtual := entradaInstrucao;
-						state <= arithmetic;
-					end if;
-					
-				when arithmetic =>
-					
-					--ADD
-					--entradaInstrucao(7) = '0' registrador registrador
-					if (instrucaoAtual(6) = '1') then
-						if (entradaInstrucao(7) = '0')then
-							sinalDataBus <= "111";
-							if (entradaInstrucao(6 downto 0) = "000000") then -- AL e AH
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0001";
-							elsif (entradaInstrucao(6 downto 0) = "0000001") then -- AL e BH
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0000010") then -- AL e BL
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0010";
-							elsif (entradaInstrucao(6 downto 0) = "0000011") then -- AL e CH
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0101";
-							elsif (entradaInstrucao(6 downto 0) = "0000100") then -- AL e CL
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0100";
-							elsif (entradaInstrucao(6 downto 0) = "0000101") then -- AL e DH
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0111";
-							elsif (entradaInstrucao(6 downto 0) = "0000110") then -- AL e DL
-								saidaRG1 <= "0000";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0000111") then -- AH e BH
-								saidaRG1 <= "0001";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0001000") then -- AH e BL
-								saidaRG1 <= "0001";
-								saidaRG2 <= "0010";
-							elsif (entradaInstrucao(6 downto 0) = "0001001") then -- AH e CH
-								saidaRG1 <= "0001";
-								saidaRG2 <= "0101";
-							elsif (entradaInstrucao(6 downto 0) = "0001010") then -- AH e CL
-								saidaRG1 <= "0001";
-								saidaRG2 <= "0100";
-							elsif (entradaInstrucao(6 downto 0) = "0001011") then -- AH e DH
-								saidaRG1 <= "0001";
-								saidaRG2 <= "0111";
-							elsif (entradaInstrucao(6 downto 0) = "0001100") then -- AH e DL
-								saidaRG1 <= "0001";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0001101") then -- BL e BH
-								saidaRG1 <= "0010";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0001110") then -- BL e AL
-								saidaRG1 <= "0010";
-								saidaRG2 <= "0000";
-							elsif (entradaInstrucao(6 downto 0) = "0001111") then -- BL e CH
-								saidaRG1 <= "0010";
-								saidaRG2 <= "0101";
-							elsif (entradaInstrucao(6 downto 0) = "0010000") then -- BL e CL
-								saidaRG1 <= "0010";
-								saidaRG2 <= "0100";
-							elsif (entradaInstrucao(6 downto 0) = "0010001") then -- BL e DH
-								saidaRG1 <= "0010";
-								saidaRG2 <= "0111";
-							elsif (entradaInstrucao(6 downto 0) = "0010010") then -- BL e DL
-								saidaRG1 <= "0010";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0010011") then -- BH e AH
-								saidaRG1 <= "0011";
-								saidaRG2 <= "0001";
-							elsif (entradaInstrucao(6 downto 0) = "0010100") then -- BH e AL
-								saidaRG1 <= "0011";
-								saidaRG2 <= "0000";
-							elsif (entradaInstrucao(6 downto 0) = "0010101") then -- BH e CH
-								saidaRG1 <= "0011";
-								saidaRG2 <= "0101";
-							elsif (entradaInstrucao(6 downto 0) = "0010110") then -- BH e CL
-								saidaRG1 <= "0011";
-								saidaRG2 <= "0100";
-							elsif (entradaInstrucao(6 downto 0) = "0010111") then -- BH e DH
-								saidaRG1 <= "0011";
-								saidaRG2 <= "0111";
-							elsif (entradaInstrucao(6 downto 0) = "0011000") then -- BH e DL
-								saidaRG1 <= "0011";
-								saidaRG2 <= "0110";							
-							elsif (entradaInstrucao(6 downto 0) = "0001101") then -- CL e CH
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0101";
-							elsif (entradaInstrucao(6 downto 0) = "0001110") then -- CL e AL
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0000";
-							elsif (entradaInstrucao(6 downto 0) = "0001111") then -- CL e AH
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0001";
-							elsif (entradaInstrucao(6 downto 0) = "0010000") then -- CL e BL
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0010";
-							elsif (entradaInstrucao(6 downto 0) = "0010001") then -- CL e BH
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0010010") then -- CL e DL
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0010010") then -- CL e DH
-								saidaRG1 <= "0100";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0010011") then -- CH e CL
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0100";
-							elsif (entradaInstrucao(6 downto 0) = "0010011") then -- CH e AH
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0001";
-							elsif (entradaInstrucao(6 downto 0) = "0010100") then -- CH e AL
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0000";
-							elsif (entradaInstrucao(6 downto 0) = "0010101") then -- CH e BH
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0010110") then -- CH e BL
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0010";
-							elsif (entradaInstrucao(6 downto 0) = "0010111") then -- CH e DH
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0111";
-							elsif (entradaInstrucao(6 downto 0) = "0011000") then -- CH e DL
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0110";	
-							elsif (entradaInstrucao(6 downto 0) = "0011001") then -- DL e DH
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0111";
-							elsif (entradaInstrucao(6 downto 0) = "0011010") then -- DL e AL
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0000";
-							elsif (entradaInstrucao(6 downto 0) = "0011011") then -- DL e AH
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0001";
-							elsif (entradaInstrucao(6 downto 0) = "0011100") then -- DL e BL
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0010";
-							elsif (entradaInstrucao(6 downto 0) = "0011101") then -- DL e BH
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0011110") then -- DL e DL
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0011111") then -- DL e DH
-								saidaRG1 <= "0110";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0100000") then -- DH e DL
-								saidaRG1 <= "0111";
-								saidaRG2 <= "0110";
-							elsif (entradaInstrucao(6 downto 0) = "0100001") then -- DH e AH
-								saidaRG1 <= "0111";
-								saidaRG2 <= "0001";
-							elsif (entradaInstrucao(6 downto 0) = "0100010") then -- DH e AL
-								saidaRG1 <= "0101";
-								saidaRG2 <= "0000";
-							elsif (entradaInstrucao(6 downto 0) = "0100011") then -- DH e BH
-								saidaRG1 <= "0111";
-								saidaRG2 <= "0011";
-							elsif (entradaInstrucao(6 downto 0) = "0100100") then -- DH e BL
-								saidaRG1 <= "0111";
-								saidaRG2 <= "0010";
-							elsif (entradaInstrucao(6 downto 0) = "0100101") then -- DH e CH
-								saidaRG1 <= "0111";
-								saidaRG2 <= "0101";
-							elsif (entradaInstrucao(6 downto 0) = "0100110") then -- DH e CL
-								saidaRG1 <= "0111";
-								saidaRG2 <= "0100";
-							end if;
-						end if;
-						sinalEscritaRT1 <= '1';
-						state <= exe;
-					end if;
-					state <= readin;
-
+				IncrementaPC <= '0';
+				if(QueueVazia = '1') then
+					state <= boot;
+				else
+					LeituraQueue <= '1'; --sinal para ler instruçao da queue
+					state <= fetch;
+				end if;
 				
+				when fetch =>
+					if (entradaInstrucao = "00000010" ) then --ADD registrador registrador 16
+						instrucaoAtual := entradaInstrucao;
+						instrucaoReal <= "00010000";
+						if(QueueVazia = '1') then
+							state <= fetch;
+						else
+							LeituraQueue <= '1'; --sinal para ler instruçao da queue
+							state <= arithmetic161;
+						end if;
+					end if;
+					
+				when arithmetic161 =>
+
+					if entradaInstrucao(7 downto 4) = "0000" then
+						saidaRG1 <= "1000";
+						elsif entradaInstrucao(7 downto 4) = "0001" then
+						saidaRG1 <= "1001";
+						elsif entradaInstrucao(7 downto 4) = "0010" then
+						saidaRG1 <= "1010";
+						elsif entradaInstrucao(7 downto 4) = "0011" then
+						saidaRG1 <= "1011";
+						elsif entradaInstrucao(7 downto 4) = "0100" then
+						saidaRG1 <= "1100";
+						elsif entradaInstrucao(7 downto 4) = "0101" then
+						saidaRG1 <= "1101";
+						elsif entradaInstrucao(7 downto 4) = "0110" then
+						saidaRG1 <= "1110";
+						elsif entradaInstrucao(7 downto 4) = "0111" then
+						saidaRG1 <= "1111";
+					end if;
+					
+					if entradaInstrucao(7 downto 4) = "0000" then
+						saidaRG2<= "1000";
+						elsif entradaInstrucao(7 downto 4) = "0001" then
+						saidaRG2<= "1001";
+						elsif entradaInstrucao(7 downto 4) = "0010" then
+						saidaRG2<= "1010";
+						elsif entradaInstrucao(7 downto 4) = "0011" then
+						saidaRG2<= "1011";
+						elsif entradaInstrucao(7 downto 4) = "0100" then
+						saidaRG2<= "1100";
+						elsif entradaInstrucao(7 downto 4) = "0101" then
+						saidaRG2<= "1101";
+						elsif entradaInstrucao(7 downto 4) = "0110" then
+						saidaRG2<= "1110";
+						elsif entradaInstrucao(7 downto 4) = "0111" then
+						saidaRG2<= "1111";
+					end if;
+					
+					
+											
+						saidaDataBUS <= "000";
+						LeituraQueue <= '0';
+						destino <= entradaInstrucao(7 downto 4);
+						sinalEscritaRT1 <= '1'; --sinal para escrever no regitrador temporario 1 
+						state <= arithmetic162;
+				
+				when arithmetic162 =>
+						if entradaInstrucao(3 downto 0) = "0000" then
+							saidaRG1 <= "1000";
+							elsif entradaInstrucao(3 downto 0) = "0001" then
+							saidaRG1 <= "1001";
+							elsif entradaInstrucao(3 downto 0) = "0010" then
+							saidaRG1 <= "1010";
+							elsif entradaInstrucao(3 downto 0) = "0011" then
+							saidaRG1 <= "1011";
+							elsif entradaInstrucao(3 downto 0) = "0100" then
+							saidaRG1 <= "1100";
+							elsif entradaInstrucao(3 downto 0) = "0101" then
+							saidaRG1 <= "1101";
+							elsif entradaInstrucao(3 downto 0) = "0110" then
+							saidaRG1 <= "1110";
+							elsif entradaInstrucao(3 downto 0) = "0111" then
+							saidaRG1 <= "1111";
+						end if;
+						
+						if entradaInstrucao(3 downto 0) = "0000" then
+							saidaRG2<= "1000";
+							elsif entradaInstrucao(3 downto 0) = "0001" then
+							saidaRG2<= "1001";
+							elsif entradaInstrucao(3 downto 0) = "0010" then
+							saidaRG2<= "1010";
+							elsif entradaInstrucao(3 downto 0) = "0011" then
+							saidaRG2<= "1011";
+							elsif entradaInstrucao(3 downto 0) = "0100" then
+							saidaRG2<= "1100";
+							elsif entradaInstrucao(3 downto 0) = "0101" then
+							saidaRG2<= "1101";
+							elsif entradaInstrucao(3 downto 0) = "0110" then
+							saidaRG2<= "1110";
+							elsif entradaInstrucao(3 downto 0) = "0111" then
+							saidaRG2<= "1111";
+						end if;
+						
+											
+						saidaDataBUS <= "001";
+						sinalEscritaRT1 <= '0';
+						sinalEscritaRT2 <= '1'; --sinal para escrever no regitrador temporario 2
+						
+						state <= exe;
+
+				when resposta =>
+					sinalEscritaRG1 <= '1';
+					sinalEscritaRG2 <= '1';
+					
+					if destino = "0000" then
+							EntradaRG1<= "0000";
+							elsif destino = "0001" then
+							EntradaRG1<= "0010";
+							elsif destino = "0010" then
+							EntradaRG1<= "0100";
+							elsif destino = "0011" then
+							EntradaRG1<= "0110";
+							elsif destino = "0100" then
+							EntradaRG1<= "1000";
+							elsif destino = "0101" then
+							EntradaRG1<= "1101";
+							elsif destino = "0110" then
+							EntradaRG1<= "1110";
+							elsif destino = "0111" then
+							EntradaRG1<= "1111";
+						end if;
+						
+						if destino = "0000" then
+							EntradaRG2<= "0001";
+							elsif destino = "0001" then
+							EntradaRG2<= "0011";
+							elsif destino = "0010" then
+							EntradaRG2<= "0101";
+							elsif destino = "0011" then
+							EntradaRG2<= "0111";
+							elsif destino = "0100" then
+							EntradaRG2<= "1001";
+							elsif destino = "0101" then
+							EntradaRG2<= "1100";
+							elsif destino = "0110" then
+							EntradaRG2<= "1110";
+							elsif destino = "0111" then
+							EntradaRG2<= "1111";
+						end if;
+				
+				
+					SaidaDataBUS <= "010";
+					state <= final;
+					
+				when final =>
+					sinalEscritaRG1 <= '0'; 
+					sinalEscritaRG2 <= '0';
+					sinalEscritaRT1 <= '0';
+					sinalEscritaRT2 <= '0';
+					IncrementaPC <= '1';
+					
+
 				when exe => --Parte final da instruçao 
 				
-					OPULA <= instrucaoAtual;-- passa a instruçao pra ula
+					OPULA <= instrucaoReal;-- passa a instruçao pra ula
 					WFlag <= '1'; --habilita escrita no registrador de flag
 					
-					state <= fetch;
+					state <= resposta;
 				when readin =>
 					--AAA
 					if(instrucaoAtual = "00000001") then
