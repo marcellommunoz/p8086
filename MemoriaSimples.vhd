a6aa508c955c7e6bbe5d5700309e95ec8f621ddc
gabrielsosoares <gsdsoares@inf.ufpel.edu.br>
2018-03-04 14:22:12 -0300

    MemoriaSimples

--- MemoriaSimples.vhd
+++ MemoriaSimples.vhd
@@ -5,12 +5,12 @@ USE ieee.std_logic_signed.all ;
 ENTITY MemoriaSimples IS PORT(
  	 Dado: in std_LOGIC_VECTOR(15 downto 0);
 	 Ender: in std_LOGIC_VECTOR(19 downto 0);
-    ByteControl, clk, w, ControleDadoInstru, reset: IN STD_LOGIC;
+    clk, reset: IN STD_LOGIC;
 	 QueueFull : in std_LOGIC;
 	 EscritaQueue : out std_LOGIC;
 	 IncrementaPC : out std_Logic;
 	 ControleBIURegister1, ControleBIURegister2 : out std_logic_vector(2 downto 0);
-	 SaidaQueue, teste: out std_LOGIC_VECTOR(7 downto 0);
+	 SaidaQueue: out std_LOGIC_VECTOR(7 downto 0);
     SaidaRegs: out std_LOGIC_VECTOR(15 downto 0)
 );
 END MemoriaSimples;
@@ -20,6 +20,7 @@ ARCHITECTURE comportamento OF MemoriaSimples IS
 
 signal addr : std_logic_vector(15 downto 0);
 signal Smemoria, data :std_logic_vector(7 downto 0);
+signal w: std_LOGIC;
 
 component Memory IS
 	PORT
@@ -33,7 +34,7 @@ component Memory IS
 end component;
 
 begin
-	
+	w <= '0';
 	ControleBIURegister1 <= "000";
 	ControleBIURegister2 <= "100";
 	addr <= Ender(15 downto 0);
